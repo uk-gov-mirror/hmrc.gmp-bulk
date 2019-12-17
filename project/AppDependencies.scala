5a3845c3be6fe0f8042662f11cdf79f36fd0e137
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2019-12-17 13:02:05 +0000

    [CU][DLS-1386]: Bootstrap upgrade and other plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "simple-reactivemongo" % "7.20.0-play-25",
-    "org.reactivemongo" %% "reactivemongo-iteratees" % "0.16.4",
+    "org.reactivemongo" %% "reactivemongo-iteratees" % "0.17.1",
     "uk.gov.hmrc" %% "bootstrap-play-25" % "5.1.0",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
     "com.typesafe.akka" %% "akka-contrib" % "2.5.23",
-    "uk.gov.hmrc" %% "play-scheduling" % "6.0.0",
+    "uk.gov.hmrc" %% "play-scheduling" % "7.1.0-play-25",
     "uk.gov.hmrc" %% "mongo-lock" % "6.15.0-play-25",
     "uk.gov.hmrc" %% "reactive-circuit-breaker" % "3.3.0",
     "uk.gov.hmrc" %% "tax-year" % "0.6.0",
-    "uk.gov.hmrc" %% "auth-client" % "2.26.0-play-25"
+    "uk.gov.hmrc" %% "auth-client" % "2.32.0-play-25"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -24,7 +24,7 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1",
     "org.pegdown" % "pegdown" % "1.6.0",
     "uk.gov.hmrc" %% "reactivemongo-test" % "4.15.0-play-25",
-    "org.reactivemongo" %% "reactivemongo-iteratees" % "0.16.1",
+    "org.reactivemongo" %% "reactivemongo-iteratees" % "0.17.1",
     "com.typesafe.akka" %% "akka-testkit" % "2.5.23",
     "org.mockito" % "mockito-all" % "1.9.5",
     "uk.gov.hmrc" %% "tax-year" % "0.6.0",
