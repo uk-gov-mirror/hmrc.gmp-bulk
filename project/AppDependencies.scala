8d50a58f29a84afca389d7c041a5f9c9ac49b84f
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-01-22 15:07:01 +0000

    [CU][DLS-1839]: Library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,10 +10,10 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
     "com.typesafe.akka" %% "akka-contrib" % "2.5.23",
-    "uk.gov.hmrc" %% "play-scheduling" % "7.1.0-play-26",
-    "uk.gov.hmrc" %% "mongo-lock" % "6.15.0-play-26",
+    "uk.gov.hmrc" %% "play-scheduling" % "7.2.0-play-26",
+    "uk.gov.hmrc" %% "mongo-lock" % "6.18.0-play-26",
     "uk.gov.hmrc" %% "reactive-circuit-breaker" % "3.3.0",
-    "uk.gov.hmrc" %% "tax-year" % "0.6.0",
+    "uk.gov.hmrc" %% "tax-year" % "1.0.0",
     "uk.gov.hmrc" %% "auth-client" % "2.32.0-play-26",
     "com.typesafe.play" %% "play-json-joda" % "2.6.10"
   )
@@ -30,7 +30,7 @@ object AppDependencies {
         "org.scalamock" %% "scalamock" % "3.6.0",
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
         "org.pegdown" % "pegdown" % "1.6.0",
-        "uk.gov.hmrc" %% "reactivemongo-test" % "4.15.0-play-26",
+        "uk.gov.hmrc" %% "reactivemongo-test" % "4.16.0-play-26",
         "org.reactivemongo" %% "reactivemongo-iteratees" % "0.18.6",
         "com.typesafe.akka" %% "akka-testkit" % "2.5.23",
         "org.mockito" % "mockito-all" % "1.9.5",
